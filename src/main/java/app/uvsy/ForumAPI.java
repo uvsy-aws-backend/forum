96c001e55fb65ced27d78474437fdb1447ab84a4
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2020-10-21 23:51:50 -0300

    Remove unused imports

--- src/main/java/app/uvsy/ForumAPI.java
+++ src/main/java/app/uvsy/ForumAPI.java
@@ -1,19 +1,8 @@
 package app.uvsy;
 
 import app.uvsy.controllers.publication.PublicationController;
-import app.uvsy.database.DBConnection;
-import app.uvsy.model.db.PublicationDB;
-import app.uvsy.model.db.PublicationTagDB;
-import app.uvsy.model.db.TagDB;
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.DaoManager;
-import com.j256.ormlite.stmt.QueryBuilder;
-import com.j256.ormlite.support.ConnectionSource;
 import org.github.serverless.api.ServerlessApiHandler;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 public class ForumAPI extends ServerlessApiHandler {
